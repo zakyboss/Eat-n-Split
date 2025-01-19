b156e76740fe2253e1e7204ecbe9f2e8f8b99bf6
Zakyboss <Boochimo8@gmail.com>
2025-01-19 16:35:43 +0300

    Added logic to toggle display of add friend form and select form

--- eat-n-split/src/AFriend.js
+++ eat-n-split/src/AFriend.js
@@ -1,14 +1,18 @@
-import './index.css';
-export default function AFriend({data}){
-    const {id, firstName, lastName,payments,photo}= data;
-    return(
-        <li  className='item'>
-          <img src={`${photo}`} alt={firstName} id='image'/>
+export default function AFriend({ data, onSelectFriend }) {
+    const { id, firstName, lastName, payments, photo } = data;
+
+    // Debugging
+    console.log("Photo URL:", photo);  // Check if the photo URL is correct
+console.log(photo)
+    return (
+        <li className="item">
+            <img src={photo} alt={firstName} id="image" />
             <span>
-                <h2>{firstName} {lastName}</h2>
-                <h3 style={{color:'green'}}>Owes You $300</h3>
+                <h2 style={{ color: "goldenrod" }}>{firstName} {lastName}</h2>
+                <h3 style={{ color: "green" }}>Owes You $300</h3>
             </span>
-                <button id='selectBtn'>Select</button>
+            <button id="selectBtn" onClick={onSelectFriend}>Select</button>
+            <button id="deleteBtn" style={{ backgroundColor: "red", color: "white" }}>Delete</button>
         </li>
-    )
+    );
 }
