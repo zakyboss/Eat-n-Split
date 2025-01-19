85c9f189f3be8c9fdd8b429b19504b2e0679c74c
Zakyboss <Boochimo8@gmail.com>
2025-01-19 16:40:08 +0300

    Receiving props and passing them to corresonding friend components

--- eat-n-split/src/Friends.js
+++ eat-n-split/src/Friends.js
@@ -2,11 +2,12 @@ import './index.css';
 import FriendList from './FriendList';
 import DeleteFriend from './DeleteFriend';
 import AddFriend from './AddFriend';
-export default function Friends({data}){
+export default function Friends({data, onSelectFriend ,onClick,onAddFriend,displayAddForm , names}){
     return (
-        <div>
-            <AddFriend/>
-            <FriendList data={data}/>
+        <div className='friendsContainer'>
+            <FriendList data={data} onSelectFriend={onSelectFriend}/>
+            <AddFriend onClick={onClick} onAddFriend={onAddFriend} displayAddForm={displayAddForm} names={names}/>
+
             <DeleteFriend/>
         </div>
     )
