68bc02e99e1edd80eba802b2452c7e9ec0eaab4b
Zakyboss <Boochimo8@gmail.com>
2025-01-19 16:37:01 +0300

    Passed necessary props to AddAFriend

--- eat-n-split/src/FriendList.js
+++ eat-n-split/src/FriendList.js
@@ -1,12 +1,12 @@
 import './index.css';
 import AFriend from './AFriend';
 
-export default function FriendList({data}){
+export default function FriendList({data , onSelectFriend}){
 return (
     <div>
         <ul>
         {data.map(friend=>
-        <AFriend data={friend}/>
+        <AFriend data={friend} onSelectFriend={onSelectFriend}/>
       
       )}  
               </ul>
